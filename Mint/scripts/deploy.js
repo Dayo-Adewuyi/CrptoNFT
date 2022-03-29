58e9e41b7b81a91d2e6d493925ba36f781564cca
dayolovesruby <dayoadewuyi@protonmail.com>
2022-03-29 12:38:02 +0100

    ce la vie

---
+++ Mint/scripts/deploy.js
@@ -0,0 +1,40 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+
+async function main() {
+  // Address of the whitelist contract that you deployed in the previous module
+  const whitelistContract = "0xb2DF62Ff73b4b02d10e03CE1bd510B2E03620Ec1";
+  // URL from where we can extract the metadata for a Crypto Dev NFT
+  const metadataURL = "https://nft-collection-sneh1999.vercel.app/api/";
+  /*
+  A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+  so cryptoDevsContract here is a factory for instances of our CryptoDevs contract.
+  */
+  const cryptoDevsContract = await ethers.getContractFactory("CryptoDevs");
+
+  // deploy the contract
+  const deployedCryptoDevsContract = await cryptoDevsContract.deploy(
+    metadataURL,
+    whitelistContract
+  );
+
+  // print the address of the deployed contract
+  console.log(
+    "Crypto Devs Contract Address:",
+    deployedCryptoDevsContract.address
+  );
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
